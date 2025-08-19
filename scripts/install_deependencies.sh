8e58ca391cd3003b856c609c9f83abf8a6933c22
Emmanuel Nwokeocha <nwokeochaemma01@gmail.com>
2025-08-19 23:34:16 +0100

    deploymennt shell scripts

---
+++ scripts/install_deependencies.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+sudo yum install tomcat -y
+sudo yum -y install httpd
+sudo cat << EOF > /etc/httpd/conf.d/tomcat_manager.conf
+<VirtualHost *:80>
+  ServerAdmin root@localhost
+  ServerName app.nextwork.com
+  DefaultType text/html
+  ProxyRequests off
+  ProxyPreserveHost On
+  ProxyPass / http://localhost:8080/java-web-app-cicd/
+  ProxyPassReverse / http://localhost:8080/java-web-app-cicd/
+</VirtualHost>
+EOF
